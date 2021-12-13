572ef4197cd07de5c87b88272da652a19ab01b41
Patricio-Arias <patricio.arias@virginiogomez.cl>
2021-12-13 01:20:06 -0300

    GPS ubicacion y marcadores

--- app/src/main/java/com/example/certamen2/MainActivity.java
+++ app/src/main/java/com/example/certamen2/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.certamen2;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void onCLick(View v){
+        startActivity(new Intent(this, MapsActivity.class));
     }
 }
