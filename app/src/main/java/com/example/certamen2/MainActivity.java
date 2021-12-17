e034e4391db0ce14d54970ee897bc65afab0ce5b
Patricio-Arias <patricio.arias@virginiogomez.cl>
2021-12-17 02:21:43 -0300

    GPS ubicacion y marcadores Final

--- app/src/main/java/com/example/certamen2/MainActivity.java
+++ app/src/main/java/com/example/certamen2/MainActivity.java
@@ -3,17 +3,89 @@ package com.example.certamen2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+    EditText titulo, descripcion;
+    double latitud, longitud; FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        titulo = findViewById(R.id.textTitulo);
+        descripcion = findViewById(R.id.textDescripcion);
+
+        latitud = getIntent().getDoubleExtra("latitud", 0);
+        longitud = getIntent().getDoubleExtra("longitud", 0);
+
+        inicializarFirebase();
+
     }
+
+
+
+
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+    }
+
+    public void cancelar(View v) {
+        Intent returnData = new Intent();
+        returnData.putExtra("agregarMarcador", false);
+        setResult(RESULT_CANCELED,returnData);
+        finish();
+    }
+
+
+
+    public void guardar(View v){
+        String  tituloDb = titulo.getText().toString();
+        String  descripcionDB = descripcion.getText().toString();
+
+
+        Localizacion posicion = new Localizacion();
+        posicion.setIdLocalizacion(UUID.randomUUID().toString());
+        posicion.setTitulo(tituloDb);
+        posicion.setDescripcion(descripcionDB);
+        posicion.setLatitud(latitud);
+        posicion.setLongitud(longitud);
+
+
+        databaseReference.child("Marcador").child(posicion.getIdLocalizacion()).setValue(posicion);
+        Toast.makeText(this, "agregado correctamente", Toast.LENGTH_SHORT).show();
+
+        Intent returnData = new Intent();
+        returnData.putExtra("agregarMarcador", true);
+        returnData.putExtra("titulo", tituloDb);
+        returnData.putExtra("descripcion", descripcionDB);
+        setResult(RESULT_OK,returnData);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     public void onCLick(View v){
         startActivity(new Intent(this, MapsActivity.class));
+
     }
 }
--- app/src/main/java/com/example/certamen2/MapsActivity.java
+++ app/src/main/java/com/example/certamen2/MapsActivity.java
@@ -8,6 +8,11 @@ import android.content.pm.PackageManager;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -20,11 +25,29 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private List<Localizacion> listaLocalizacion = new ArrayList<>();
+    private boolean cargarMarcadoresDeFirebase = true;
+    private boolean agregarMarcador = false;
+    String titulo;
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +61,62 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         getLocalizacion();
+        inicializarFirebase();
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == RESULT_OK && result.getData() != null){
+                    Intent intent = result.getData();
+                    titulo = intent.getStringExtra("titulo");
+                    agregarMarcador = intent.getBooleanExtra("addMarker", false);
+                }
+                else if (result.getResultCode() == RESULT_CANCELED){
+                    agregarMarcador = result.getData().getBooleanExtra("agregarMarcador",false);
+                }
+            }
+        });
     }
 
+    private void inicializarFirebase() {
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
 
+    }
+    private void listaDatos(GoogleMap mMap) {
+        databaseReference.child("Marcador").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaLocalizacion.clear();
+                for (DataSnapshot objSnapshot : dataSnapshot.getChildren()){
+                    Localizacion marcador = objSnapshot.getValue(Localizacion.class);
+                    listaLocalizacion.add(marcador);
+                    System.out.println(marcador.toString());
+                }
+                System.out.println(">>datos recuperados de firebase: "+listaLocalizacion.size());
+
+                System.out.println("cargarMarcadoresDeFirebase: "+cargarMarcadoresDeFirebase);
+                System.out.println("cantidad de datos en la lista: "+ listaLocalizacion.size());
+                if (cargarMarcadoresDeFirebase){
+                    for (int i = 0; i < listaLocalizacion.size(); i++) {
+                        Localizacion localizacion =  listaLocalizacion.get(i);
+                        LatLng punto = new LatLng(localizacion.getLatitud(), localizacion.getLongitud());
+                        mMap.addMarker(new MarkerOptions().position(punto).title(localizacion.getTitulo()));
+                    }
+                    cargarMarcadoresDeFirebase = false;
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    
     private void getLocalizacion() {
         int permiso = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
         if (permiso == PackageManager.PERMISSION_DENIED) {
@@ -64,6 +140,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
+        listaDatos(mMap);
+
         mMap.setMyLocationEnabled(true);
 
         //mMap.getUiSettings().setMyLocationButtonEnabled(false);
@@ -75,7 +153,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         @SuppressLint("MissingPermission") android.location.Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
         double userLat = lastKnownLocation.getLatitude();
         double userLong = lastKnownLocation.getLongitude();
-        LatLng miUbicacion = new LatLng(userLat, userLong);
+        LatLng miUbicacion = new LatLng(userLat, userLong);         //captura ubicacion
         mMap.moveCamera(CameraUpdateFactory.newLatLng(miUbicacion));
         CameraPosition cameraPosition = new CameraPosition.Builder()
                 .target(miUbicacion)
@@ -85,15 +163,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .build();
         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
 
+        //click largo marcador
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng point) {
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                mMap.addMarker(new MarkerOptions()
-                        .position(point)
-                        .title("You are here"));
+                Intent myIntent = new Intent(getApplicationContext(), MainActivity.class);
+                myIntent.putExtra("latitud", point.latitude);
+                myIntent.putExtra("longitud", point.longitude);
+                activityResultLauncher.launch(myIntent);
+                if (agregarMarcador) {
+                    mMap.addMarker(new MarkerOptions().position(point).title(titulo));
+                }
             }
         });
     }
+
 
 }
